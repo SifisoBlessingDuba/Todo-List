bee6d151c42e53a5e72391e361e073c322d6ca61
Sifiso Blessing Duba <Sifisoblessing75@gmail.com>
2025-03-09 13:05:41 +0200

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "*************************",
+    authDomain: "todo-list-c1bbe.firebaseapp.com",
+    projectId: "todo-list-c1bbe",
+    storageBucket: "todo-list-c1bbe.appspot.com",
+    messagingSenderId: "***************",
+    appId: "***************************",
+    measurementId: "****************"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+export { auth, db };
